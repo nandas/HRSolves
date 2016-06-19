616ccc146118e5d2151dd1674fc8ef1405ea54de
nandas <nandakumararaja@gmail.com>
2016-06-19 14:24:03 -0400

    HR LL 1st solve

--- HRSolves/src/com/ll/Node.java
+++ HRSolves/src/com/ll/Node.java
@@ -4,4 +4,18 @@ public class Node
 {
    int data;
    Node next;
+   
+   public Node(int x){
+		data=x;
+	}
+   
+   void appendToEnd(int data){
+		Node  newnode=new Node(data);
+		Node head=this;
+		while(head.next != null){
+			head=head.next;
+		}
+		head.next=newnode;
+		
+	}
 }
